aeb9ed43e45c06a8f8c1f483b29262b0a78afc32
juanp1995 <juanp1995@gmail.com>
2016-03-20 21:28:54 -0600

    Final

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Core core = new Core(8001, "192.168.43.45", 80, true);
+		Core core = new Core(8001, "192.168.43.44", 80, true);
 
 
 
